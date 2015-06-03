a556de7c136004633c3c852018f46395c532fdf6
David Beck <dbeck@starschema.net>
2015-06-03 11:19:40 +0200

    CI and submodule changes; minor scaffolding

---
+++ src/queue/queue.hh
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace virtdb { namespace queue {
+
+  class queue {
+  public:
+    class writer {};
+    class reader {};
+  };
+}}
--- test/queue_test.cc
+++ test/queue_test.cc
@@ -1,1 +1,23 @@
-int main() { return 0; }
+#include <gtest/gtest.h>
+#include <queue/queue.hh>
+
+using namespace virtdb::queue;
+
+namespace virtdb { namespace test {
+  class QueueTest : public ::testing::Test { };
+}}
+
+using namespace virtdb::test;
+
+TEST_F(QueueTest, Dummy)
+{
+  queue q;
+}
+
+int main(int argc, char ** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  auto ret = RUN_ALL_TESTS();
+  return ret;
+  return 0;
+}
